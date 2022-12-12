2273ac83d4bf32b8e33554cf70ddbf8417188adf
MathieuChayer <80339719+MathieuChayer@users.noreply.github.com>
2022-12-11 19:51:10 -0500

    day11

---
+++ DAY 11/day11p2.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+class Monkey:
+
+  def __init__(self, name, items, operation, test, if_true, if_false):
+    self.name = name
+    self.items = items
+    self.operation = operation
+    self.test = test
+    self.if_true = if_true
+    self.if_false = if_false
+    self.inspections = 0
+
+  def inspect_and_throw(self):
+      worry_lvl = eval(self.operation.replace("old",str(self.items[0])))
+      worry_lvl = int(worry_lvl)
+
+      if worry_lvl%self.test == 0:
+          destination_monkey = self.if_true
+      else:
+          destination_monkey = self.if_false
+
+      return worry_lvl, destination_monkey
+
+
+# Read inputs
+inputs = open('day11.txt')
+inputs = inputs.read().strip().split("\n")
+
+# Parse input and make monkeys
+Monkeys = []
+for i in range(len(inputs)):
+    if "Monkey" in inputs[i]:
+        name = int(inputs[i].split()[1][0])
+        items = [int(j) for j in inputs[i+1][18:].split(", ")]
+        operation = inputs[i+2].split("= ")[1]
+        test = int(inputs[i+3].split("by")[1])
+        if_true = int(inputs[i+4].split("monkey")[1])
+        if_false = int(inputs[i+5].split("monkey")[1])
+        Monkeys.append(Monkey(name, items, operation, test, if_true, if_false))
+
+
+# You can reduce the worry_level by modulating against the divisors of the monkeys
+cycle = 1
+for i in range(len(Monkeys)):
+    cycle*=Monkeys[i].test
+
+# Compute rounds
+round = 1
+max_rounds = 10000
+while round <= max_rounds :
+    for i in range(len(Monkeys)):
+        while Monkeys[i].items != []:
+            worry_lvl, destination_monkey = Monkeys[i].inspect_and_throw()
+            worry_lvl = worry_lvl%cycle
+            Monkeys[destination_monkey].items.append(worry_lvl)
+            Monkeys[i].items.pop(0)
+            Monkeys[i].inspections+=1
+
+    print(round,"/",max_rounds)
+    round+=1
+
+# Print answers
+inspections = []
+for i in range(len(Monkeys)):
+    inspections.append(Monkeys[i].inspections)
+
+print("Monkey Business after 20 rounds : ", sorted(inspections,reverse=True)[0]*sorted(inspections,reverse=True)[1])
